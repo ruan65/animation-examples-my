4b49466f269d5f5a433d20cf39d1cec8a02f0d5b
ruan65 <ruan65@gmail.com>
2015-06-25 00:29:03 +0300

    some stage

--- app/src/main/java/premiumapp/org/propertyanimationtutplus/ExperimentActivity.java
+++ app/src/main/java/premiumapp/org/propertyanimationtutplus/ExperimentActivity.java
@@ -1,32 +1,29 @@
 package premiumapp.org.propertyanimationtutplus;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 
 public class ExperimentActivity extends ActionBarActivity {
-
-    ImageView iv1, iv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_experiment);
 
-        iv1 = (ImageView) findViewById(R.id.imageView);
-        iv2 = (ImageView) findViewById(R.id.imageView2);
+        Bitmap tile = BitmapFactory.decodeResource(getResources(), R.drawable.trans_background);
 
+        BitmapDrawable drawableTile = new BitmapDrawable(tile);
 
-        setDrawableToImageView(iv1, R.drawable.rect);
-        setDrawableToImageView(iv2, R.drawable.rect1);
+        drawableTile.setTileModeXY(Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
 
-    }
+        findViewById(R.id.v1).setBackgroundDrawable(drawableTile);
 
-    private void setDrawableToImageView(ImageView iv, int resId) {
-
-        iv.setImageResource(resId);
     }
 }
