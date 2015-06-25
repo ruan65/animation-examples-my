6a13160e3c86cbbcf72a1ff4afb41a8514217881
ruan65 <ruan65@gmail.com>
2015-06-25 21:06:33 +0300

    margin movement implemented

--- app/src/main/java/premiumapp/org/propertyanimationtutplus/TimerActivity.java
+++ app/src/main/java/premiumapp/org/propertyanimationtutplus/TimerActivity.java
@@ -3,13 +3,11 @@ package premiumapp.org.propertyanimationtutplus;
 import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.view.ViewTreeObserver;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -20,17 +18,16 @@ import butterknife.InjectView;
 
 public class TimerActivity extends AppCompatActivity implements View.OnTouchListener {
 
-    @InjectView(R.id.tv1) TextView tv1;
-    @InjectView(R.id.tv2) TextView tv2;
-    @InjectView(R.id.tv3) TextView tv3;
-    @InjectView(R.id.tv4) TextView tv4;
-    @InjectView(R.id.containerRL) RelativeLayout container;
-
-    LayoutParams lp1, lp2, lp3, lp4;
+    @InjectView(R.id.v0) TextView v0;
+    @InjectView(R.id.v1) TextView v1;
+    @InjectView(R.id.v2) TextView v2;
+    @InjectView(R.id.v3) TextView v3;
+    @InjectView(R.id.containerRL) RelativeLayout mFrame;
 
     private GestureDetectorCompat mDetector;
 
-    TextView currentTouchedView = null;
+    TextView mCurrentTouchedView = null;
+    TextView mOpenedView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +36,10 @@ public class TimerActivity extends AppCompatActivity implements View.OnTouchList
 
         ButterKnife.inject(this);
 
-        tv1.setOnTouchListener(this);
-        tv2.setOnTouchListener(this);
-        tv3.setOnTouchListener(this);
-        tv4.setOnTouchListener(this);
+        v0.setOnTouchListener(this);
+        v1.setOnTouchListener(this);
+        v2.setOnTouchListener(this);
+        v3.setOnTouchListener(this);
 
         mDetector = new GestureDetectorCompat(this, new LocalGestureListener());
 
@@ -51,37 +48,53 @@ public class TimerActivity extends AppCompatActivity implements View.OnTouchList
 
     private void setViewHeights() {
 
-        container.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+        mFrame.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
 
             @Override
             public void onGlobalLayout() {
-                container.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                mFrame.getViewTreeObserver().removeOnGlobalLayoutListener(this);
 
-                int heightPixels = container.getHeight();
+                int heightPixelsDp = mFrame.getHeight();
 
+                int maxH = heightPixelsDp / 2;
 
-                lp1 = tv1.getLayoutParams();
-                lp2 = tv2.getLayoutParams();
-                lp3 = tv3.getLayoutParams();
-                lp4 = tv4.getLayoutParams();
+                Log.d("ml", "heightPixelsDp = " + heightPixelsDp);
 
-                changeViewHeight(tv1, lp1, heightPixels * 2 / 5);
-                changeViewHeight(tv2, lp2, heightPixels / 5);
-                changeViewHeight(tv3, lp3, heightPixels / 5);
-                changeViewHeight(tv4, lp4, heightPixels / 5);
+                initViewHeightAndMargin(v0, maxH, 0);
+                initViewHeightAndMargin(v1, maxH, maxH);
+                initViewHeightAndMargin(v2, maxH, maxH + maxH / 3);
+                initViewHeightAndMargin(v3, maxH, maxH + maxH * 2 / 3);
+
+                mOpenedView = v0;
             }
         });
     }
 
-    private void changeViewHeight(View v, LayoutParams lp, int h) {
-        lp.height = h;
+    private void initViewHeightAndMargin(View v, int height, int marginTop) {
+
+        ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+
+        lp.height = height;
+        lp.topMargin = marginTop;
         v.setLayoutParams(lp);
     }
+
+    private void changeMarginTop(View v, int delta) {
+
+        ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+
+        lp.topMargin += delta;
+
+        v.setLayoutParams(lp);
+    }
+
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
 
-        currentTouchedView = (TextView) v;
+        if (v == mOpenedView) return false;
+
+        mCurrentTouchedView = (TextView) v;
         mDetector.onTouchEvent(event);
 
         return true;
@@ -92,9 +105,9 @@ public class TimerActivity extends AppCompatActivity implements View.OnTouchList
         @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
 
-            Log.d("ml", "y1 - y2 = " + (e1.getY() - e2.getY()));
-
-
+            if (mCurrentTouchedView.getId() != R.id.v0) {
+                changeMarginTop(mCurrentTouchedView, (int) (-distanceY * 0.7));
+            }
 
             return true;
         }
@@ -102,8 +115,8 @@ public class TimerActivity extends AppCompatActivity implements View.OnTouchList
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
 
-            Log.d("ml", "fl x = " + e2.getX() + " Y = " + e2.getY());
-            Log.d("ml", "fl x1 = " + e1.getX() + " Y1 = " + e1.getY());
+//            Log.d("ml", "fl x = " + e2.getX() + " Y = " + e2.getY());
+//            Log.d("ml", "fl x1 = " + e1.getX() + " Y1 = " + e1.getY());
             return true;
         }
     }
