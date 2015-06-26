1f4bfc19f12291dcafcdf4be82d8c657930cd66d
ruan65 <ruan65@gmail.com>
2015-06-26 05:13:46 +0300

    touch events

--- app/src/main/java/premiumapp/org/propertyanimationtutplus/TimerActivity.java
+++ app/src/main/java/premiumapp/org/propertyanimationtutplus/TimerActivity.java
@@ -1,16 +1,12 @@
 package premiumapp.org.propertyanimationtutplus;
 
 import android.os.Bundle;
-import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.ViewGroup.MarginLayoutParams;
 import android.view.ViewTreeObserver;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -18,16 +14,23 @@ import butterknife.InjectView;
 
 public class TimerActivity extends AppCompatActivity implements View.OnTouchListener {
 
-    @InjectView(R.id.v0) TextView v0;
-    @InjectView(R.id.v1) TextView v1;
-    @InjectView(R.id.v2) TextView v2;
-    @InjectView(R.id.v3) TextView v3;
+    @InjectView(R.id.v0)
+    View v0;
+    @InjectView(R.id.v1)
+    View v1;
+    @InjectView(R.id.v2)
+    View v2;
+    @InjectView(R.id.v3)
+    View v3;
     @InjectView(R.id.containerRL) RelativeLayout mFrame;
 
-    private GestureDetectorCompat mDetector;
+    View mCurrentTouchedView = null;
 
-    TextView mCurrentTouchedView = null;
-    TextView mOpenedView;
+    View mOpenedView;
+
+    int v1h, v2h, v3h, v1l, v2l, v3l; // highest and lowest positions of all movable views
+
+    double delta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +43,6 @@ public class TimerActivity extends AppCompatActivity implements View.OnTouchList
         v1.setOnTouchListener(this);
         v2.setOnTouchListener(this);
         v3.setOnTouchListener(this);
-
-        mDetector = new GestureDetectorCompat(this, new LocalGestureListener());
 
         setViewHeights();
     }
@@ -58,66 +59,107 @@ public class TimerActivity extends AppCompatActivity implements View.OnTouchList
 
                 int maxH = heightPixelsDp / 2;
 
-                Log.d("ml", "heightPixelsDp = " + heightPixelsDp);
+                v1l = maxH;
+                v2l = maxH + maxH / 3;
+                v3l = maxH + maxH * 2 / 3;
+
+                v1h = v1l - maxH * 2 / 3;
+                v2h = v2l - maxH * 2 / 3;
+                v3h = v3l - maxH * 2 / 3;
 
                 initViewHeightAndMargin(v0, maxH, 0);
-                initViewHeightAndMargin(v1, maxH, maxH);
-                initViewHeightAndMargin(v2, maxH, maxH + maxH / 3);
-                initViewHeightAndMargin(v3, maxH, maxH + maxH * 2 / 3);
+                initViewHeightAndMargin(v1, maxH, v1l);
+                initViewHeightAndMargin(v2, maxH, v2l);
+                initViewHeightAndMargin(v3, maxH, v3l);
 
                 mOpenedView = v0;
             }
         });
     }
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        if (v == mOpenedView) return false;
+
+        mCurrentTouchedView = v;
+
+        MarginLayoutParams lp = getMarginLayoutParams(mCurrentTouchedView);
+
+        switch (event.getAction()) {
+
+            case MotionEvent.ACTION_DOWN:
+
+                int position = lp.topMargin;
+
+                double y = event.getRawY();
+
+                delta = y - position;
+
+                return true;
+
+            case MotionEvent.ACTION_MOVE:
+
+                double value = event.getRawY() - delta;
+
+                switch (mCurrentTouchedView.getId()) {
+
+                    case R.id.v1:
+
+                        if (lp.topMargin >= v1h && lp.topMargin <= v1l) {
+
+                            setMarginTop(mCurrentTouchedView, (int) value);
+                        }
+                        return adjustTopMargin(lp, v1h, v1l);
+
+                    case R.id.v2:
+
+                        if (lp.topMargin >= v2h && lp.topMargin <= v2l) {
+
+                            setMarginTop(mCurrentTouchedView, (int) value);
+                        }
+                        return adjustTopMargin(lp, v2h, v2l);
+
+                    case R.id.v3:
+
+                        if (lp.topMargin >= v3h && lp.topMargin <= v3l) {
+
+                            setMarginTop(mCurrentTouchedView, (int) value);
+                        }
+                        return adjustTopMargin(lp, v3h, v3l);
+
+                    default:
+                        return true;
+                }
+        }
+
+        return true;
+    }
+
     private void initViewHeightAndMargin(View v, int height, int marginTop) {
 
-        ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+        MarginLayoutParams lp = getMarginLayoutParams(v);
 
         lp.height = height;
         lp.topMargin = marginTop;
         v.setLayoutParams(lp);
     }
 
-    private void changeMarginTop(View v, int delta) {
+    private void setMarginTop(View v, int value) {
 
-        ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+        MarginLayoutParams lp = getMarginLayoutParams(v);
 
-        lp.topMargin += delta;
+        lp.topMargin = value;
 
         v.setLayoutParams(lp);
     }
 
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-
-        if (v == mOpenedView) return false;
-
-        mCurrentTouchedView = (TextView) v;
-        mDetector.onTouchEvent(event);
-
-        return true;
+    private MarginLayoutParams getMarginLayoutParams(View v) {
+        return (MarginLayoutParams) v.getLayoutParams();
     }
 
-    private class LocalGestureListener extends GestureDetector.SimpleOnGestureListener {
-
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-
-            if (mCurrentTouchedView.getId() != R.id.v0) {
-                changeMarginTop(mCurrentTouchedView, (int) (-distanceY * 0.7));
-            }
-
-            return true;
-        }
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-
-//            Log.d("ml", "fl x = " + e2.getX() + " Y = " + e2.getY());
-//            Log.d("ml", "fl x1 = " + e1.getX() + " Y1 = " + e1.getY());
-            return true;
-        }
+    private boolean adjustTopMargin(MarginLayoutParams lp, int h, int l) {
+        lp.topMargin = lp.topMargin < h ? lp.topMargin = h : lp.topMargin > l ? l : lp.topMargin;
+        return true;
     }
 }
