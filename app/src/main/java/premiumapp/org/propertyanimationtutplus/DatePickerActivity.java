826826b1d038ce28dfcb6d98d84dcad382037163
ruan65 <ruan65@gmail.com>
2016-04-02 09:38:14 +0300

    some refactoring

--- app/src/main/java/premiumapp/org/propertyanimationtutplus/DatePickerActivity.java
+++ app/src/main/java/premiumapp/org/propertyanimationtutplus/DatePickerActivity.java
@@ -1,13 +1,87 @@
 package premiumapp.org.propertyanimationtutplus;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class DatePickerActivity extends AppCompatActivity {
+
+    @Bind(R.id.et_date)
+    EditText etDate;
+
+    @Bind(R.id.tv_info)
+    TextView tvInfo;
+
+    private final Calendar calendar = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_picker);
+        ButterKnife.bind(this);
+
+//        etDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View v, boolean hasFocus) {
+//                if (hasFocus) {
+//                    new DatePickerDialog(DatePickerActivity.this, new DatePickerDialog.OnDateSetListener() {
+//                        @Override
+//                        public void onDateSet(DatePicker picker, int year, int monthOfYear, int dayOfMonth) {
+//
+//                            etDate.setText(picker.getDayOfMonth() + "/" + picker.getMonth() +
+//                                    "/" + picker.getYear());
+//
+//                        }
+//                    }, 2016, 1, 1);
+//                }
+//            }
+//        });
+
+        etDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                new DatePickerDialog(DatePickerActivity.this, new DateSetListener(), year, month, day).show();
+            }
+        });
+
+    }
+
+    private class DateSetListener implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public void onDateSet(DatePicker picker, int year, int monthOfYear, int dayOfMonth) {
+
+            String myFormat = "dd MMMM yyyy";
+            SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.getDefault());
+
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, monthOfYear);
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+            etDate.setText(sdf.format(calendar.getTime()));
+        }
+    }
+
+    @OnClick(R.id.btn_show_date)
+    public void show(View v) {
+        tvInfo.setText(etDate.getText().toString());
     }
 }
--- app/src/main/java/premiumapp/org/propertyanimationtutplus/SomeActivity.java
+++ app/src/main/java/premiumapp/org/propertyanimationtutplus/SomeActivity.java
@@ -1,8 +1,7 @@
 package premiumapp.org.propertyanimationtutplus;
 
-import android.graphics.drawable.TransitionDrawable;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,39 +9,28 @@ import android.widget.ImageView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
+public class SomeActivity extends AppCompatActivity implements View.OnTouchListener {
 
-public class SomeActivity extends ActionBarActivity implements View.OnTouchListener {
+    @Bind(R.id.imageView) ImageView wheel;
 
-    @Bind(R.id.imageView)
-    ImageView wheel;
+    @Bind(R.id.imageView2) ImageView wheel2;
 
-    @Bind(R.id.imageView2)
-    ImageView wheel2;
-
-    @Bind(R.id.toggle_image)
-    ImageView trView;
-
-    float dX, dY;
-
-    TransitionDrawable transitionDrawable;
+    private float dX, dY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_some);
-
         ButterKnife.bind(this);
 
-        transitionDrawable = (TransitionDrawable) getResources().getDrawable(R.drawable.transition);
         wheel.setOnTouchListener(this);
         wheel2.setOnTouchListener(this);
-        trView.setImageDrawable(transitionDrawable);
     }
 
     @Override
     public boolean onTouch(View view, MotionEvent event) {
 
-        switch (event.getActionMasked()) {
+        switch (event.getAction()) {
 
             case MotionEvent.ACTION_DOWN:
 
@@ -54,41 +42,15 @@ public class SomeActivity extends ActionBarActivity implements View.OnTouchListe
                 view.animate()
                         .x(event.getRawX() + dX)
                         .y(event.getRawY() + dY)
-                        .alpha(.5f)
                         .setDuration(0)
                         .start();
                 break;
-
-            case MotionEvent.ACTION_UP:
-            break;
             default:
                 return false;
         }
         return true;
     }
-
     public void recreate(View view) {
-
         recreate();
     }
-
-    public void trClick(View view) {
-
-        transitionDrawable.startTransition(5000);
-    }
 }
-
-
-//                ObjectAnimator.ofFloat(v, "x", event.getRawX() + dX)
-//                        .setDuration(0)
-//                        .start();
-//
-//                ObjectAnimator.ofFloat(v, "y", event.getRawY() + dY )
-//                        .setDuration(0)
-//                        .start();
-//
-//                v.animate()
-//                        .translationX(event.getRawX() + dX)
-//                        .translationY(event.getRawY() + dY)
-//                        .setDuration(0)
-//                        .start();
